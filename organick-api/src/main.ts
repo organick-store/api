23f2a47a35aa1d32823087898783686800a2639d
kreslavskiy <mishak1000236@gmail.com>
2023-06-05 19:32:15 +0300

    Changed port x2

--- organick-api/src/main.ts
+++ organick-api/src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5000);
+  await app.listen(4000);
 }
 bootstrap();
